02b3f5e2e9289cb65e6c7a301ce130907c3cddfb
spencer2585 <spencer2585@gmail.com>
2026-09-06 23:23:34 -0400

    Interfaces to support Multiple games as modules seperate from the main mod

---
+++ HaloAP_DLL/Games/IGameModule.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+struct IGameEvents;
+
+struct IGameModule
+{
+    virtual bool InstallHooks(IGameEvents* events) = 0;
+    
+    virtual bool UninstallHooks() = 0;
+    
+    virtual void SendItem(int localID)= 0;
+    
+    virtual void ReceiveDeathlink() = 0;
+    
+    virtual GameInfo GetGameInfo() = 0;
+    
+    virtual MissionList GetMissionList() = 0;
+    
+    virtual SkullData GetSkullData() = 0;
+    
+    virtual ~IGameModule() = default;
+};
---
+++ HaloAP_DLL/IGameEvents.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+
+struct IGameEvents
+{
+    virtual void SendLocation(int locationID) = 0;
+    
+    virtual void SendDeathlink() = 0;
+    
+    virtual ~IGameEvents() = default;
+};
