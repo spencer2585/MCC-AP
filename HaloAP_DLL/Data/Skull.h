1665f11a555df430c3381421afe3e6495077c7c7
spencer2585 <spencer2585@gmail.com>
2026-09-07 22:57:21 -0400

    Fill in Skull data enum

--- HaloAP_DLL/Data/Skull.h
+++ HaloAP_DLL/Data/Skull.h
@@ -2,4 +2,38 @@
 
 #include <cstdint>
 
-enum class Skull: uint8_t {};
+enum class Skull: uint8_t {
+    Anger,
+    Black_Eye,
+    Blind,
+    Catch,
+    Eye_Patch,
+    Famine,
+    Fog,
+    Foreign,
+    Iron,
+    Mythic,
+    Recession,
+    Thats_Just_Wrong,
+    Thunderstorm,
+    Tough_Luck,
+    Boom,
+    Cowbell,
+    Feather,
+    Ghost,
+    Grunt_Birthday_Party,
+    Grunt_Funeral,
+    IWHBYD,
+    Malfuction,
+    Pinata,
+    Prophet_Birthday_Party,
+    SO_ANGRY,
+    Sputnik,
+    Swarm,
+    They_Come_Back,
+    Acrophobia,
+    Bandana,
+    Bonded_Pair,
+    Envy,
+    Scarab
+};
