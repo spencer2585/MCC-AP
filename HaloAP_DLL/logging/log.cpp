c108495118c612ad3ec9c340bcdbc9cc22504c67
spencer2585 <spencer2585@gmail.com>
2026-09-14 11:27:22 -0400

    Restructure logging to get location of log and support future log file writting

---
+++ HaloAP_DLL/logging/log.cpp
@@ -0,0 +1,30 @@
+﻿#include "log.h"
+
+#include <format>
+#include <source_location>
+#include <string>
+#include <string_view>
+
+namespace haloap
+{
+    struct formatWithLoc
+    {
+        std::string_view str;
+        std::source_location location;
+    
+        formatWithLoc(std::string_view s, std::source_location loc = std::source_location::current()):str(s), location(loc){}
+    };
+    template <typename... Args>
+    void log(formatWithLoc fmt, Args... args)
+    {
+        //Construct our message
+        std::string message = std::vformat(fmt.str, std::make_format_args(args...));
+        //append location to beginning
+        std::string finalMessage = std::format("[{}:{}] {}\n", fmt.location.file_name(), fmt.location.line(), message );
+        //write to console
+        #ifdef HALOAP_ENABLE_CONSOLE
+        fputs(finalMessage.c_str(), stdout);
+        #endif
+        //TODO: write to file
+    }
+}
